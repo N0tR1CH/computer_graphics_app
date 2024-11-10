bcc2b548b9a58e1105edf3abfd74123189412b24
N0tR1CH <damianrichter58@gmail.com>
2024-11-10 21:45:55 +0100

    Rename button for clearance

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -114,8 +114,8 @@
 						}
 					}}
 					type="button"
-					class="my-2 mb-2 rounded-lg bg-purple-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900"
-					>Dodaj</button
+					class="my-2 mb-2 w-full rounded-lg bg-purple-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900"
+					>Dodaj komentarz do zapisywanego pliku</button
 				>
 			</div>
 			{#each comments as comment}
