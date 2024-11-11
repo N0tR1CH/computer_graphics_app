c5c2a232529445a2a75ca2ce9ece608676d143a2
N0tR1CH <damianrichter58@gmail.com>
2024-11-12 00:31:37 +0100

    Remove web server

--- main.go
+++ main.go
@@ -1,11 +1,7 @@
 package main
 
 import (
-	"context"
 	"embed"
-	"log"
-	"net/http"
-	"time"
 
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
@@ -21,17 +17,6 @@ var assets embed.FS
 var icon []byte
 
 func main() {
-	// Create a server for image hosting
-	server := &http.Server{
-		Addr:    ":3000",
-		Handler: http.StripPrefix("/images/", http.FileServer(http.Dir("./images"))),
-	}
-
-	go func() {
-		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			log.Fatal(err)
-		}
-	}()
 	// Create an instance of the app structure
 	app := NewApp()
 	worker := NewWorker(app)
@@ -50,14 +35,6 @@ func main() {
 		},
 		BackgroundColour: &options.RGBA{R: 0, G: 0, B: 0, A: 0},
 		OnStartup:        app.startup,
-		OnShutdown: func(ctx context.Context) {
-			ctxShutDown, cancel := context.WithTimeout(ctx, 5*time.Second)
-			defer cancel()
-			if err := server.Shutdown(ctxShutDown); err != nil {
-				log.Fatalf("Server shutdown failed: +%v", err)
-			}
-			log.Println("Server exited properly")
-		},
 		Bind: []any{
 			app,
 			worker,
